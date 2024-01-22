807c44e3d2dbfdbb7253b56801d156b904532df3
Gui Prá <gui@guiprav.com>
2024-01-22 13:45:50 -0300

    Internal AppCtrl refactor

--- controllers/App.js
+++ controllers/App.js
@@ -13,55 +13,57 @@ class AppCtrl {
 
   post = (action, ...args) => {
     location.href.includes('debug=1') && console.log('appCtrl.post:', action, args);
-    this[action](...args);
+    this.actions[action](...args);
     d.update();
   };
 
-  loadTemplates() { this.state.templates = templateRepo.loadTemplates() }
-  loadCandidates() { this.state.candidates = candidateRepo.loadCandidates() }
+  actions = {
+    loadTemplates: () => this.state.templates = templateRepo.loadTemplates(),
+    loadCandidates: () => this.state.candidates = candidateRepo.loadCandidates(),
 
-  saveTemplate(x, data) {
-    templateRepo.saveTemplate(x, data);
-    this.post('loadTemplates');
-  }
+    saveTemplate: (x, data) => {
+      templateRepo.saveTemplate(x, data);
+      this.post('loadTemplates');
+    },
 
-  newTemplate(name) {
-    this.post('saveTemplate', nanoid(), { name, html: '' });
-    this.post('loadTemplates');
-  }
+    newTemplate: name => {
+      this.post('saveTemplate', nanoid(), { name, html: '' });
+      this.post('loadTemplates');
+    },
 
-  renameTemplate(x, name) {
-    this.post('saveTemplate', x, { ...templateRepo.loadTemplate(x), name });
-    this.post('loadTemplates');
-  }
+    renameTemplate: (x, name) => {
+      this.post('saveTemplate', x, { ...templateRepo.loadTemplate(x), name });
+      this.post('loadTemplates');
+    },
 
-  deleteTemplate(x) {
-    templateRepo.deleteTemplate(x);
-    if (this.state.openEntity === `template:${x}`) { this.state.openEntity = null }
-    this.post('loadTemplates');
-  }
+    deleteTemplate: (x) => {
+      templateRepo.deleteTemplate(x);
+      if (this.state.openEntity === `template:${x}`) { this.state.openEntity = null }
+      this.post('loadTemplates');
+    },
 
-  openTemplate(x) { this.state.openEntity = `template:${x}` }
+    openTemplate: x => this.state.openEntity = `template:${x}`,
 
-  saveCandidate(x, data) {
-    candidateRepo.saveCandidate(x, data);
-    this.post('loadCandidates');
-  }
+    saveCandidate: (x, data) => {
+      candidateRepo.saveCandidate(x, data);
+      this.post('loadCandidates');
+    },
 
-  newCandidate() {
-    let id = nanoid();
-    this.post('saveCandidate', id, { firstName: '', lastName: '', template: 'builtin-1', experiences: [], education: [], skills: [] });
-    this.post('loadCandidates');
-    this.post('openCandidate', id)
-  }
+    newCandidate: () => {
+      let id = nanoid();
+      this.post('saveCandidate', id, { firstName: '', lastName: '', template: 'builtin-1', experiences: [], education: [], skills: [] });
+      this.post('loadCandidates');
+      this.post('openCandidate', id)
+    },
 
-  deleteCandidate(x) {
-    candidateRepo.deleteCandidate(x);
-    if (this.state.openEntity === `candidate:${x}`) { this.state.openEntity = null }
-    this.post('loadCandidates');
-  }
+    deleteCandidate: (x) => {
+      candidateRepo.deleteCandidate(x);
+      if (this.state.openEntity === `candidate:${x}`) { this.state.openEntity = null }
+      this.post('loadCandidates');
+    },
 
-  openCandidate(x) { this.state.openEntity = `candidate:${x}` }
+    openCandidate: x => this.state.openEntity = `candidate:${x}`,
+  };
 }
 
 let appCtrl = new AppCtrl();
